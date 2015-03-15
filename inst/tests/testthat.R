4b541a747c533e8df922b23f44514ce5f43f5830
Gergely Daróczi <daroczig@rapporter.net>
2015-03-15 01:05:10 -0700

    add a test to our "tested" function

---
+++ inst/tests/testthat.R
@@ -0,0 +1,6 @@
+## load some dependencies
+library(testthat)
+library(codecov.R)
+
+## test package
+test_package('codecov.R')
---
+++ inst/tests/testthat/test-allwehave.R
@@ -0,0 +1,4 @@
+context('Testing functions...')
+test_that('We got numbers', {
+    expect_that(tested(5), equals(5))
+})
